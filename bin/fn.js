a1870e9eae1d86662c3454cc3d2721e51c50479d
coffeephilic <62822251+coffeephilic@users.noreply.github.com>
2020-03-29 11:30:55 +0800

    Minimum viable product
    TODO:
      adjust trigger vaules
      implement SequentialArray library, including contour functions
      revise UI based on designer feedback

---
+++ bin/fn.js
@@ -0,0 +1,16 @@
+(function(){
+	let head=document.getElementsByTagName("head")[0];
+	function loadScript(path){
+		let script=document.createElement("script");
+		script.src=path;
+		head.appendChild(script);
+	}
+	function loadStyle(path){
+		//TODO
+	}
+	loadScript("/lib/js/audioAnalysis.lib.js");
+	loadScript("/lib/js/quantize.fn.js");
+	loadScript("/lib/js/color.lib.js");
+	loadScript("/lib/js/numericalArray.lib.js");
+	//loadScript("/lib/js/sequentialArray.lib.js");
+}());
